6d37852f00723b8801dadd8fea87e01b1a3b5b09
bkbkbk2588 <48542383+bkbkbk2588@users.noreply.github.com>
2020-05-22 17:33:09 +0900

    연습문제

---
+++ Programmers/Level1/자연수 뒤집어 배열로 만들기/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+	public int[] solution(long n) {
+		String str = Long.toString(n);
+		int[] answer = new int[str.length()];
+		for (int i = 0; i < str.length(); i++) {
+			answer[i] = (int) (n % 10);
+			n = n / 10;
+		}
+		return answer;
+	}
+}
